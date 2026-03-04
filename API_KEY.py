0cd64a2b1acf6d38750490627d41e398cb5f5e38
Shubham3791 <shubhamjadhav37910@gmail.com>
2026-03-04 15:18:28 +0530

    Modify API keys in API_KEY.py
    Updated API keys for Command AI, News API, and Weather API.

--- API_KEY.py
+++ API_KEY.py
@@ -1,3 +1,3 @@
-Command_AI="AIfAm2_ZUBFZ5DmdC_3p_YZ-_1zo"
-News_API_KEY = "pub_b3636a26bc96931cd8ad7"
-Weather_API_KEY = "0265c2d73cf139b"
+Command_AI="AIZ-_1zo"
+News_API_KEY = "p96931cd"
+Weather_API_KEY = "02
